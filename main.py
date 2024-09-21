7005d7f80c0e12930d5dfb5364e17973e7967770
AdeliaShevaPutri <adeliashevaputri@gmail.com>
2024-09-21 15:15:35 +0700

    latihan calon proggamer handal

---
+++ main.py
@@ -0,0 +1,45 @@
+# nilai = int(input("masukkan nilai: "))
+# print(nilai)
+
+# nilai = nilai + 5
+# print(nilai)
+
+# if (nilai > 90 and nilai < 100):
+#     print("SELAMAT ANDA LULUS")
+# elif (nilai > 80):
+#     print("SELAMAT ANDA LULUS, DENGAN NILAI YANG MEMUASKAN")
+# elif (nilai > 70):
+#     print("LULUS TAPI DEKET JURANG")
+# else:
+#     print("ANDA MASIH BELUM LULUS TES, SEMANGAT DAN COBA LAGI")
+
+#  for loop
+# for i in range(10, 0, -2):
+#     print(i)  
+#     print("HELLO WORLD")  
+
+# angka = 10
+
+# while (angka > 1):
+#     print("alloww")
+#     angka -= 1 # angka = angka -1
+
+# [FUNCTION]
+# def sayHello():
+#     print("HELLO SHEVAAA")
+
+# sayHello()
+# def sayHello(param_nama, param_umur):
+#     print(f"Hello {param_nama}, umur : {param_umur}")
+#     print("Selamat Pagii")
+
+#ANONYMOUS FUNCTION: lambda
+fungsi_kali2 = lamda :2 * 2 # type: ignore
+
+print(fungsi_2kali2()) # type: ignore
+
+fungsi_kurang4 =  lambda param_angka :param_angka - 4
+
+angka = 10
+print(fungsi_kurang4(angka))
+
